70d3e7cf9dd7015c520477ffa18ccdf9f51e6387
hiten52 <hitensolanki010@gmail.com>
2022-12-05 15:09:25 +0530

    Parser.js

---
+++ Parser.js
@@ -0,0 +1,39 @@
+import keywords from "./keywords.js"
+
+class Parser {
+    constructor(source) {
+        this.source = source.trim().split(/\r?\n/)
+        this.tokens = []
+        this.createTokens()
+    }
+
+    createTokens() {
+        this.source.forEach((line) => {
+            line = line.trim().split(/\s+/)
+            this.createToken(line)
+        });
+
+        return this.tokens
+    }
+
+    createToken(line) {
+        let word = line[0]
+        switch (word) {
+            case keywords.ADD || keywords.SUBTRACT || keywords.NEGATIVE 
+              || keywords.NOT || keywords.OR || keywords.AND || keywords.GREATER_THAN
+              || keywords.LESS_THAN || keywords.EQUAL :
+                this.tokens.push({type:"ARITHMETIC", value:word})
+                break
+            case keywords.POP :
+                this.tokens.push({type:"POP", segment: line[1], i:line[2]})
+                break
+            case keywords.PUSH :
+                this.tokens.push({type:"PUSH", segment: line[1], i:line[2]})
+                break
+            default :
+                throw new Error(`Invalid keyword ${word}`)
+        }
+    }   
+}
+
+export default Parser
