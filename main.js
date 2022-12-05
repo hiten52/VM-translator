c53f74a53ff081c576a60729e68ff00baddf5f0c
hiten52 <hitensolanki010@gmail.com>
2022-12-05 13:45:59 +0530

    create output file

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
-const fileName = process.argv[2]
+import path from 'node:path'
+import {readFile, writeFile} from 'node:fs/promises'
 
-import {readFile} from 'node:fs/promises'
-const source = await readFile(fileName).then(file => file.toString())
+const inputFileName = path.basename(process.argv[2])
+const outputFileName = inputFileName.slice(0,-3) + ".asm"
+
+const source = await readFile(inputFileName)
+                .then(file => file.toString())
+                .catch(err => err.message)
+
+const output = await writeFile(outputFileName, "translated code here")
+                .catch(err => err.message)
 
 
