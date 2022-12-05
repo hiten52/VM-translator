6974ff3e28f5110c8c366560a4467d15ce211324
hiten52 <hitensolanki010@gmail.com>
2022-12-05 15:09:06 +0530

    add keywords.js

---
+++ keywords.js
@@ -0,0 +1,15 @@
+const keywords = {
+    ADD: "add",
+    SUBTRACT: "sub", 
+    NEGATIVE: "neg",
+    EQUAL: "eq",
+    GREATER_THAN: "gt",
+    LESS_THAN: "lt",
+    AND: "and",
+    OR: "or",
+    NOT: "not",
+    POP: "pop",
+    PUSH: "push"
+}
+
+export default keywords
