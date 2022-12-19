cbea9f108f6885e1b6aca7e93f885cea2184611a
hiten52 <hitensolanki010@gmail.com>
2022-12-19 12:18:45 +0530

    Add code writer for bootstrap code

--- CodeWriter.js
+++ CodeWriter.js
@@ -30,6 +30,74 @@ class CodeWriter {
         return code
     }
 
+    writeBootstrap() {
+        var codeblock = ""
+        codeblock += 
+        `
+        @256
+        D=A
+        @SP
+        M=D
+
+        @Bootstrap$ret
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+
+        @LCL
+        D=M
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+
+        @ARG
+        D=M
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+
+        @THIS
+        D=M
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+
+        @THAT
+        D=M
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+
+        @SP
+        D=M
+        @5
+        D=D-A
+        @ARG
+        M=D
+
+        @SP
+        D=M
+        @LCL
+        M=D
+
+        @Sys.init
+        0;JMP
+
+        (@Bootstrap$ret)
+        `
+    }
+
     writePopAndPush(token) {
         var codeblock = ""
         if(token.segment === "constant") {
