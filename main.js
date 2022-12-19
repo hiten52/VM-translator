a09614da1b7763de62c19571a7c21813eb40d8a4
hiten52 <hitensolanki010@gmail.com>
2022-12-19 15:51:34 +0530

    Add input reader for directory

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 #!/usr/bin/env node
 import path from 'node:path'
-import {readFile, writeFile} from 'node:fs/promises'
+import { lstatSync } from 'node:fs'
+import {readFile, writeFile, readdir} from 'node:fs/promises'
 
 import Parser from './Parser.js'
 import CodeWriter from './CodeWriter.js'
 
-const inputFileName = path.basename(process.argv[2])
-const outputFileName = inputFileName.slice(0,-3) + ".asm"
+var files = []
 
-const source = await readFile(inputFileName)
-                .then(file => file.toString())
-                .catch(err => err.message)
+const input = path.basename(process.argv[2])
+const isDir = lstatSync(input).isDirectory()
+var outputFileName
+var inputPath = input
 
-let tokens = new Parser(source).createTokens()
-let code = new CodeWriter(tokens, inputFileName).writeCode()
+if(isDir) {
+    outputFileName = input + ".asm"
+    files = await readdir(input);
+} else {
+    outputFileName = input.slice(0,-3) + ".asm"
+    files.push(input)
+}
+
+var code = ""
+for (const inputFileName of files) {
+    if(inputFileName.slice(-3) !== '.vm')continue
+    if(isDir)inputPath = path.join(input, inputFileName)
+    const source = await readFile(inputPath)
+        .then(file => file.toString())
+        .catch(err => err.message)
+
+    let tokens = new Parser(source).createTokens()
+    code += new CodeWriter(tokens, inputFileName).writeCode()
+    code += '\n'
+}
 
 const output = await writeFile(outputFileName, code)
-                .catch(err => err.message)
-
-
+    .catch(err => err.message)
